5046f9efa660967bff0c7626b3ae68b7f68d82be
skybreakbb <willistaylora@gmail.com>
2023-07-13 16:20:05 -0500

    added missing foreign key in employees table

--- db/schema.sql
+++ db/schema.sql
@@ -23,5 +23,6 @@ CREATE TABLE employees (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT DEFAULT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
     FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
